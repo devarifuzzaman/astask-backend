fda65ebaf76394976ca8b5e7efca927f2320c0e7
MD Arifuzzaman <70232458+devarifuzzaman@users.noreply.github.com>
2024-11-30 12:09:56 +0600

    update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,151 +3,113 @@ const jwt = require('jsonwebtoken');
 const OTPModel = require('../models/OTPModel');
 const SendEmailUtility = require('../utility/SendEmailUtility');
 
-
-//User  Registration
+// User Registration
 exports.registration = (req, res) => {
- let reqBody = req.body;
- UserModel.create(reqBody, (err, data) => {
-  if (err) {
-   res.status(200).json({ status: "fail", data: err });
-  }
-  else {
-   res.status(200).json({ status: "Success", data: data });
+  let reqBody = req.body;
+  
+  // Validate incoming data (can be expanded)
+  if (!reqBody.email || !reqBody.password) {
+    return res.status(400).json({ status: "fail", message: "Email and password are required." });
   }
 
- })
+  // Create new user in the database
+  UserModel.create(reqBody, (err, data) => {
+    if (err) {
+      return res.status(500).json({ status: "fail", message: err.message });
+    } else {
+      return res.status(201).json({ status: "success", data: data });
+    }
+  });
 }
 
-
-//User Login
-
+// User Login
 exports.login = (req, res) => {
- let reqBody = req.body;
- UserModel.aggregate([
-  { $match: reqBody },
-  { $project: { _id: 0, email: 1, firstName: 1, lastName: 1, mobile: 1, photo: 1 } }
- ], (err, data) => {
-  if (err) {
-   res.status(404).json({ status: "fail", data: err });
-  }
-  else {
-   if (data.length > 0) {
-    let Payload = { exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), data: data[0]['email'] };
-    let token = jwt.sign(Payload, 'SecretKey123456789');
-    res.status(200).json({ status: "Success", token: token, data: data[0] });
-   }
-   else {
-    res.status(401).json({ status: "Unauthorized" });
-   }
-  }
- })
-}
+  let reqBody = req.body;
 
-
-
-//User Profile Update
-exports.profileUpdate = (req, res) => {
- let email = req.headers['email']
- let reqBody = req.body;
- UserModel.updateOne({ email: email }, reqBody, (err, data) => {
-  if (err) {
-   res.status(404).json({ status: "fail", data: err });
-  }
-  else {
-   res.status(200).json({ status: "Success", data: data });
+  // Validate request body
+  if (!reqBody.email || !reqBody.password) {
+    return res.status(400).json({ status: "fail", message: "Email and password are required." });
   }
 
- })
-}
+  // Find user by email
+  UserModel.findOne({ email: reqBody.email }, (err, user) => {
+    if (err) {
+      return res.status(500).json({ status: "fail", message: err.message });
+    }
+    
+    if (!user) {
+      return res.status(404).json({ status: "fail", message: "User not found" });
+    }
 
-//User Profile Details 
-exports.profileDetails = (req, res) => {
- let email = req.headers['email'];
- UserModel.aggregate([
-  { $match: { email: email } },
-  { $project: { _id: 1, email: 1, firstName: 1, lastName: 1, mobile: 1, photo: 1, password: 1 } }
- ], (err, data) => {
-  if (err) {
-   res.status(400).json({ status: "Fail", data: err });
+    // Password matching (Assuming hashed password is used)
+    user.comparePassword(reqBody.password, (err, isMatch) => {
+      if (err) {
+        return res.status(500).json({ status: "fail", message: err.message });
+      }
+
+      if (!isMatch) {
+        return res.status(401).json({ status: "fail", message: "Invalid credentials" });
+      }
+
+      // Create JWT token
+      const payload = {
+        userId: user._id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName
+      };
+
+      const token = jwt.sign(payload, 'your_secret_key', { expiresIn: '1h' });
+
+      return res.status(200).json({
+        status: "success",
+        message: "Login successful",
+        token: token,
+        user: {
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          mobile: user.mobile,
+          photo: user.photo
+        }
+      });
+    });
+  });
+};
+
+// OTP-based Authentication
+exports.verifyOTP = (req, res) => {
+  const { email, otp } = req.body;
+
+  if (!email || !otp) {
+    return res.status(400).json({ status: "fail", message: "Email and OTP are required." });
   }
-  else {
-   res.status(200).json({ status: "Success", data: data });
-  }
- })
 
-}
+  // Find OTP record for the email
+  OTPModel.findOne({ email: email, otp: otp }, (err, otpRecord) => {
+    if (err) {
+      return res.status(500).json({ status: "fail", message: err.message });
+    }
 
+    if (!otpRecord) {
+      return res.status(400).json({ status: "fail", message: "Invalid OTP" });
+    }
 
-exports.RecoverVerifyEmail = async (req, res) => {
- let email = req.params.email;
- let OTPCode = Math.floor(100000 + Math.random() * 900000)
- try {
-  // Email Account Query
-  let UserCount = (await UserModel.aggregate([{ $match: { email: email } }, { $count: "total" }]))
-  if (UserCount.length > 0) {
-   // OTP Insert
-   let CreateOTP = await OTPModel.create({ email: email, otp: OTPCode })
-   // Email Send
-   let SendEmail = await SendEmailUtility(email, "Your PIN Code is= " + OTPCode, "Task Manager PIN Verification")
-   res.status(200).json({ status: "success", data: SendEmail })
-  }
-  else {
-   res.status(200).json({ status: "fail", data: "No User Found" })
-  }
+    // OTP is valid, proceed with further actions (e.g., login or registration)
+    return res.status(200).json({ status: "success", message: "OTP verified successfully" });
+  });
+};
 
- } catch (e) {
-  res.status(200).json({ status: "fail", data: e })
- }
-
-}
-
-
-
-
-exports.RecoverVerifyOTP = async (req, res) => {
- let email = req.params.email;
- let OTPCode = req.params.otp;
- let status = 0;
- let statusUpdate = 1;
- try {
-  let OTPCount = await OTPModel.aggregate([{ $match: { email: email, otp: OTPCode, status: status } }, { $count: "total" }])
-  if (OTPCount.length > 0) {
-   let OTPUpdate = await OTPModel.updateOne({ email: email, otp: OTPCode, status: status }, {
-    email: email,
-    otp: OTPCode,
-    status: statusUpdate
-   })
-   res.status(200).json({ status: "success", data: OTPUpdate })
-  } else {
-   res.status(200).json({ status: "fail", data: "Invalid OTP Code" })
-  }
- }
- catch (e) {
-  res.status(200).json({ status: "fail", data: e })
- }
-}
-
-
-
-exports.RecoverResetPass = async (req, res) => {
-
- let email = req.body['email'];
- let OTPCode = req.body['OTP'];
- let NewPass = req.body['password'];
- let statusUpdate = 1;
-
- try {
-  let OTPUsedCount = await OTPModel.aggregate([{ $match: { email: email, otp: OTPCode, status: statusUpdate } }, { $count: "total" }])
-  if (OTPUsedCount.length > 0) {
-   let PassUpdate = await UserModel.updateOne({ email: email }, {
-    password: NewPass
-   })
-   res.status(200).json({ status: "success", data: PassUpdate })
-  } else {
-   res.status(200).json({ status: "fail", data: "Invalid Request" })
-  }
- }
- catch (e) {
-  res.status(200).json({ status: "fail", data: e })
- }
-}
+// Example Utility for Sending Emails (Use for OTP, registration confirmation, etc.)
+exports.sendWelcomeEmail = (email, firstName) => {
+  const subject = 'Welcome to Our Platform';
+  const message = `Hello ${firstName},\n\nThank you for registering with us. We're excited to have you!`;
+  
+  SendEmailUtility.sendEmail(email, subject, message, (err, response) => {
+    if (err) {
+      console.log("Error sending welcome email:", err);
+    } else {
+      console.log("Welcome email sent successfully:", response);
+    }
+  });
+};
